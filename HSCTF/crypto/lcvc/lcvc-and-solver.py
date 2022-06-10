7aa57b62c0aea905b1a5260129cc88c13c9f2d56
Muhammad Ichwan <michwan184@gmail.com>
2022-06-11 01:28:50 +0700

    Update lcvc-and-solver.py

--- HSCTF/crypto/lcvc/lcvc-and-solver.py
+++ HSCTF/crypto/lcvc/lcvc-and-solver.py
@@ -9,6 +9,8 @@ assert(flag[0:5]+flag[-1]=="flag{}")
 ciphertext = ""
 for character in flag[5:-1]:
     state = (15*state+18)%29
+    #Change "+" to "-"
+    #ciphertext+=alphabet[(alphabet.index(character)+state)%26]
     ciphertext+=alphabet[(alphabet.index(character)-state)%26]
 
 print("flag{%s}"  % (ciphertext) )
